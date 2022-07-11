384bbe5b624f9a897aadc61db82ad3fda4a76dd5
psp123456e <pspdsl123@gmail.com>
2022-07-11 12:22:05 +0800

    abc

--- hello orld/ViewController.swift
+++ hello orld/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+            
+    }
+     
+    @IBAction func showMessage(sender:UIButton){
+        let alertController = UIAlertController(title: "Welcom to My First App", message:" Hello World",preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title:"ok",style:UIAlertAction.Style.default,handler :nil))
+        present(alertController, animated: true ,completion: nil)
+    }
+
         // Do any additional setup after loading the view.
     }
 
+    
 
-}
 
